a31ed0ecec2975fcc42154e12f03595c9bab3139
MKhour <>
2023-12-02 13:40:13 -0500

    Updated ip script

--- jetson-ip-update.sh
+++ jetson-ip-update.sh
@@ -5,6 +5,7 @@ WEBHOOK_URL=url_goes_here
 if [ "$IFACE" = wlan0 ]; then
   # get the IPv4 address
   IP_ADDRESS=$(ifconfig wlan0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
+  PUBLIC_IP_ADDRESS=$(curl -s https://ipinfo.io/ip)
   # send a Discord message with the new IP address
-  curl -X POST $WEBHOOK_URL -H 'Content-Type: application/json' -d '{"content": "Jetson IP address: `${IPaddress}`"}'
+  curl -X POST $WEBHOOK_URL -H 'Content-Type: application/json' -d '{"content": "Jetson IP addresses:\ninternal - `${IP_ADDRESS}`\nexternal - `${PUBLIC_IP_ADDRESS}`"}'
 fi
