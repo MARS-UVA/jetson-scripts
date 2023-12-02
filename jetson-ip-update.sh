2e55894b9786a96eeba0f9892ef9ddeb3f0e9440
MKhour <>
2023-12-02 17:52:20 -0500

    Updated IP script message

--- jetson-ip-update.sh
+++ jetson-ip-update.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-# check if the interface that just had a new connection is wlan0
-if [ "$IFACE" = wlan0 ]; then
+# check if the interface that just had a new connection is wired
+if [ "$IFACE" = eth0 -o "$IFACE" = rndis0 ]; then
   # get the IPv4 address
   IP_ADDRESS=$(ifconfig wlan0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1)
-  PUBLIC_IP_ADDRESS=$(curl -s https://ipinfo.io/ip)
-  # send a Discord message with the new IP address
-  curl -X POST $WEBHOOK_URL -H "Content-Type: application/json" -d "{'content': 'Jetson IP addresses:\ninternal - `${IP_ADDRESS}`\nexternal - `${PUBLIC_IP_ADDRESS}`'}"
+  # send a Discord message to show that the Jetson is online
+  curl -X POST $WEBHOOK_URL -H "Content-Type: application/json" -d "{\"content\": \"Jetson is online!\`\`\`\nLocal IP address: \`${IP_ADDRESS}\`\`\`\"}"
 fi
